f068e92c7b13c47a2fb110c0c9c91e16e1e8d1b0
ZenithNUC <lizengzhi1998@foxmail.com>
2019-10-15 22:24:18 +0800

    2019.10.15 updata.Add GUI to zOS

--- project/boot_img_source/bootsource.c
+++ project/boot_img_source/bootsource.c
@@ -7,20 +7,57 @@ void io_store_eflags(int eflags);
 
 void init_palette(void);
 void set_palette(int start, int end, unsigned char *rgb);
+void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1);
 /*-----以上函数存在于bootsource.c-------*/
+
+#define COL8_000000		0
+#define COL8_FF0000		1
+#define COL8_00FF00		2
+#define COL8_FFFF00		3
+#define COL8_0000FF		4
+#define COL8_FF00FF		5
+#define COL8_00FFFF		6
+#define COL8_FFFFFF		7
+#define COL8_C6C6C6		8
+#define COL8_840000		9
+#define COL8_008400		10
+#define COL8_848400		11
+#define COL8_000084		12
+#define COL8_840084		13
+#define COL8_008484		14
+#define COL8_848484		15
 
 void HariMain(void)
 {	
-	int i;
-	char *p;
+	char *vram;
+	int xsize,ysize;
 
 	init_palette();
+	vram = (char *) 0xa0000;
+	xsize = 320;
+	ysize = 200;
+	
+	boxfill8(vram, xsize, COL8_840084,   0,         0,          xsize -  1, ysize - 29);
+	boxfill8(vram, xsize, COL8_C6C6C6,   0,         ysize - 28, xsize -  1, ysize - 28);
+	boxfill8(vram, xsize, COL8_FFFFFF,   0,         ysize - 27, xsize -  1, ysize - 27);
+	boxfill8(vram, xsize, COL8_C6C6C6,   0,         ysize - 26, xsize -  1, ysize -  1);
+	//桌面图案
+	boxfill8(vram, xsize, COL8_FF0000,  20,  				20, 	   150, 	   120);
+	boxfill8(vram, xsize, COL8_00FF00,  70,  				50, 	   180, 	   140);
+	boxfill8(vram, xsize, COL8_0000FF, 120,  				80, 	   210, 	   150);
+	//以下为左下角按钮
+	boxfill8(vram, xsize, COL8_FFFFFF,  3,         ysize - 24, 59,         ysize - 24);
+	boxfill8(vram, xsize, COL8_FFFFFF,  2,         ysize - 24,  2,         ysize -  4);
+	boxfill8(vram, xsize, COL8_848484,  3,         ysize -  4, 59,         ysize -  4);
+	boxfill8(vram, xsize, COL8_848484, 59,         ysize - 23, 59,         ysize -  5);
+	boxfill8(vram, xsize, COL8_000000,  2,         ysize -  3, 59,         ysize -  3);
+	boxfill8(vram, xsize, COL8_000000, 60,         ysize - 24, 60,         ysize -  3);
+	//以下为右下角按钮
+	boxfill8(vram, xsize, COL8_848484, xsize - 47, ysize - 24, xsize -  4, ysize - 24);
+	boxfill8(vram, xsize, COL8_848484, xsize - 47, ysize - 23, xsize - 47, ysize -  4);
+	boxfill8(vram, xsize, COL8_FFFFFF, xsize - 47, ysize -  3, xsize -  4, ysize -  3);
+	boxfill8(vram, xsize, COL8_FFFFFF, xsize -  3, ysize - 24, xsize -  3, ysize -  3);
 
-	p = (char *) 0xa0000;
-
-	for(i = 0; i <= 0xaffff; i++){
-		p[i] = i & 0x0f;
-	}
 	while(1){
 		io_hlt();
 	}
@@ -61,5 +98,15 @@ void set_palette(int start,int end,unsigned char *rgb){
 		rgb += 3;
 	}
 	io_store_eflags(eflags);
+	return;
+}
+
+void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1){
+	int x,y;
+	for(y = y0; y <= y1; y++){
+		for(x = x0; x<= x1; x++){
+			vram[y * xsize + x] = c;
+		}
+	}
 	return;
 }
