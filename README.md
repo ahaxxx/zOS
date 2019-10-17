bb0affa72e462bbe8cc7ff5d76065fe48b2225bf
ZenithNUC <lizengzhi1998@foxmail.com>
2019-10-17 19:31:55 +0800

    2019.10.17 updata.

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # zOS
-It's a DIY operate system.Just like a hello world program.And I will update it untimely.
+
+
+zOS是由个人开发的带有实验性质的操作系统，开发过程中参考了川和秀实先生编著的《30天自制操作系统》一书。zOS的宿命可能仅仅停留在我的电脑的虚拟机中，但是这对我个人来说则是一个宝贵的经历。由于个人工作原因，无法对开发进度每天更新，因此本项目将会在未来不定时更新进度。
+欢迎各位操作系统爱好者与我交流：liubingxing@nucosc.com
+
+zOS is an experimental operating system developed by a independent developer.
+You can content with me by email:liubingxing@nucosc.com
+
+## zOS开发日志（Development log）
+* 2019.09.18
+  * 项目开始，提交了由二进制编写的img文件
+* 2019.10.17
+  * 加入了ASCII编码的字符显示
--- project/boot_img_source/bootsource.c
+++ project/boot_img_source/bootsource.c
@@ -8,6 +8,8 @@ void io_store_eflags(int eflags);
 void init_palette(void);
 void set_palette(int start, int end, unsigned char *rgb);
 void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1);
+void init_screen(char *vram, int x, int y);
+void putfont8(char *vram, int xsize, int x, int y, char c, char *font);
 /*-----以上函数存在于bootsource.c-------*/
 
 #define COL8_000000		0
@@ -27,43 +29,39 @@ void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, i
 #define COL8_008484		14
 #define COL8_848484		15
 
+struct Bootinfo {
+	//引导信息
+	char cyls, leds, vmode, reserve;
+	short scrnx, scrny;
+	char *vram;
+};
+
 void HariMain(void)
 {	
-	char *vram;
-	int xsize,ysize;
+	struct Bootinfo *bootinfo;
+	extern char hankaku[4096];
 
 	init_palette();
-	vram = (char *) 0xa0000;
-	xsize = 320;
-	ysize = 200;
-	
-	boxfill8(vram, xsize, COL8_840084,   0,         0,          xsize -  1, ysize - 29);
-	boxfill8(vram, xsize, COL8_C6C6C6,   0,         ysize - 28, xsize -  1, ysize - 28);
-	boxfill8(vram, xsize, COL8_FFFFFF,   0,         ysize - 27, xsize -  1, ysize - 27);
-	boxfill8(vram, xsize, COL8_C6C6C6,   0,         ysize - 26, xsize -  1, ysize -  1);
-	//桌面图案
-	boxfill8(vram, xsize, COL8_FF0000,  20,  				20, 	   150, 	   120);
-	boxfill8(vram, xsize, COL8_00FF00,  70,  				50, 	   180, 	   140);
-	boxfill8(vram, xsize, COL8_0000FF, 120,  				80, 	   210, 	   150);
-	//以下为左下角按钮
-	boxfill8(vram, xsize, COL8_FFFFFF,  3,         ysize - 24, 59,         ysize - 24);
-	boxfill8(vram, xsize, COL8_FFFFFF,  2,         ysize - 24,  2,         ysize -  4);
-	boxfill8(vram, xsize, COL8_848484,  3,         ysize -  4, 59,         ysize -  4);
-	boxfill8(vram, xsize, COL8_848484, 59,         ysize - 23, 59,         ysize -  5);
-	boxfill8(vram, xsize, COL8_000000,  2,         ysize -  3, 59,         ysize -  3);
-	boxfill8(vram, xsize, COL8_000000, 60,         ysize - 24, 60,         ysize -  3);
-	//以下为右下角按钮
-	boxfill8(vram, xsize, COL8_848484, xsize - 47, ysize - 24, xsize -  4, ysize - 24);
-	boxfill8(vram, xsize, COL8_848484, xsize - 47, ysize - 23, xsize - 47, ysize -  4);
-	boxfill8(vram, xsize, COL8_FFFFFF, xsize - 47, ysize -  3, xsize -  4, ysize -  3);
-	boxfill8(vram, xsize, COL8_FFFFFF, xsize -  3, ysize - 24, xsize -  3, ysize -  3);
 
+	bootinfo = (struct Bootinfo *) 0x0ff0;
+
+	init_screen(bootinfo->vram, bootinfo->scrnx, bootinfo->scrny);
+
+	putfont8(bootinfo->vram, bootinfo->scrnx,  8, 8, COL8_FFFFFF, hankaku + 'H' * 16);
+	putfont8(bootinfo->vram, bootinfo->scrnx, 16, 8, COL8_FFFFFF, hankaku + 'E' * 16);
+	putfont8(bootinfo->vram, bootinfo->scrnx, 24, 8, COL8_FFFFFF, hankaku + 'L' * 16);
+	putfont8(bootinfo->vram, bootinfo->scrnx, 32, 8, COL8_FFFFFF, hankaku + 'L' * 16);
+	putfont8(bootinfo->vram, bootinfo->scrnx, 40, 8, COL8_FFFFFF, hankaku + 'O' * 16);
+	putfont8(bootinfo->vram, bootinfo->scrnx, 56, 8, COL8_FFFFFF, hankaku + 'z' * 16);
+	putfont8(bootinfo->vram, bootinfo->scrnx, 64, 8, COL8_FFFFFF, hankaku + 'O' * 16);
+	putfont8(bootinfo->vram, bootinfo->scrnx, 72, 8, COL8_FFFFFF, hankaku + 'S' * 16);
 	while(1){
 		io_hlt();
 	}
 }
 
 void init_palette(void){
+	//RGB颜色初始化函数，定义了RGB色值
 	static unsigned char table_rgb[16 * 3] = {
 		0x00, 0x00, 0x00,	/*  0:黑 */
 		0xff, 0x00, 0x00,	/*  1:红 */
@@ -101,12 +99,55 @@ void set_palette(int start,int end,unsigned char *rgb){
 	return;
 }
 
-void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1){
+void boxfill8(unsigned char *vram, int xsize, unsigned char color, int x0, int y0, int x1, int y1){
+	//填充一个以(x0,y0)为基准点，横向为x1-x0，纵向为y1-y0的矩形
 	int x,y;
 	for(y = y0; y <= y1; y++){
 		for(x = x0; x<= x1; x++){
-			vram[y * xsize + x] = c;
+			vram[y * xsize + x] = color;
 		}
+	}
+	return;
+}
+
+void init_screen(char *vram, int xsize, int ysize){
+	boxfill8(vram, xsize, COL8_840084,   0,         0,          xsize -  1, ysize - 29);
+	boxfill8(vram, xsize, COL8_C6C6C6,   0,         ysize - 28, xsize -  1, ysize - 28);
+	boxfill8(vram, xsize, COL8_FFFFFF,   0,         ysize - 27, xsize -  1, ysize - 27);
+	boxfill8(vram, xsize, COL8_C6C6C6,   0,         ysize - 26, xsize -  1, ysize -  1);
+	//桌面图案
+	boxfill8(vram, xsize, COL8_FF0000,  20,  				20, 	   150, 	   120);
+	boxfill8(vram, xsize, COL8_00FF00,  70,  				50, 	   180, 	   140);
+	boxfill8(vram, xsize, COL8_0000FF, 120,  				80, 	   210, 	   150);
+	//以下为左下角按钮
+	boxfill8(vram, xsize, COL8_FFFFFF,  3,         ysize - 24, 59,         ysize - 24);
+	boxfill8(vram, xsize, COL8_FFFFFF,  2,         ysize - 24,  2,         ysize -  4);
+	boxfill8(vram, xsize, COL8_848484,  3,         ysize -  4, 59,         ysize -  4);
+	boxfill8(vram, xsize, COL8_848484, 59,         ysize - 23, 59,         ysize -  5);
+	boxfill8(vram, xsize, COL8_000000,  2,         ysize -  3, 59,         ysize -  3);
+	boxfill8(vram, xsize, COL8_000000, 60,         ysize - 24, 60,         ysize -  3);
+	//以下为右下角按钮
+	boxfill8(vram, xsize, COL8_848484, xsize - 47, ysize - 24, xsize -  4, ysize - 24);
+	boxfill8(vram, xsize, COL8_848484, xsize - 47, ysize - 23, xsize - 47, ysize -  4);
+	boxfill8(vram, xsize, COL8_FFFFFF, xsize - 47, ysize -  3, xsize -  4, ysize -  3);
+	boxfill8(vram, xsize, COL8_FFFFFF, xsize -  3, ysize - 24, xsize -  3, ysize -  3);
+	return;
+}
+
+void putfont8(char *vram, int xsize, int x, int y, char color, char *font){
+	int i;
+	char *p, d;
+	for(i = 0; i < 16; i++){
+		p = vram + (y + i) * xsize + x;
+		d = font[i];
+		if ((d & 0x80) != 0) { p[0] = color; }
+		if ((d & 0x40) != 0) { p[1] = color; }
+		if ((d & 0x20) != 0) { p[2] = color; }
+		if ((d & 0x10) != 0) { p[3] = color; }
+		if ((d & 0x08) != 0) { p[4] = color; }
+		if ((d & 0x04) != 0) { p[5] = color; }
+		if ((d & 0x02) != 0) { p[6] = color; }
+		if ((d & 0x01) != 0) { p[7] = color; }
 	}
 	return;
 }
